825787fcbe4e5dfa25cdc357dc5859926c171b03
Harley Sugarman <harleysugarman@gmail.com>
2024-01-02 20:30:43 -0500

    add basic email scanning to detect dumb phrases

--- src/extension.js
+++ src/extension.js
@@ -16,6 +16,31 @@ function startExtension(gmail) {
 
   gmail.observe.on("load", () => {
     gmail.observe.on("compose", (compose) => {
+      let composeDomElement = compose.dom("body")[0];
+      composeDomElement.addEventListener("keydown", () => {
+        let composeBody = compose.body();
+        analyzeBody(composeBody);
+      });
     });
   });
 }
+
+function analyzeBody(composeBody) {
+  dumbPhrases.forEach((phrase) => {
+    if (composeBody.includes(phrase)) {
+      // TODO: show this in the email or a popup
+      console.log("found dumb phrase: " + phrase);
+    }
+  });
+}
+
+const dumbPhrases = [
+  "circle back",
+  "touch base",
+  "ping",
+  "check in",
+  "check-in",
+  "checkin",
+  "quick question",
+  "quick q",
+];
